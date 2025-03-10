49f878753301939eaa11123a7af13ea38f1a1ae7
Jays5252 <jay.aurven@gmail.com>
2025-03-10 22:38:50 +0530

    All mulutiple  element asignment

---
+++ tests/MultiLocatorAll.spec.js
@@ -0,0 +1,15 @@
+import {test, expect} from '@playwright/test'
+
+test('Click on All dropdown Elements', async({page})=>
+{
+    await page.goto('https://playwright.dev/');
+    await page.locator('//a[@role="button"]').hover();
+    let supportedLanguage =  await page.locator('//a[@target="_self"]').all();
+    for(const dropdownValue of supportedLanguage)
+    {
+        await dropdownValue.click();
+        await page.locator('//a[@role="button"]').hover();
+        //await page.waitForTimeout(3000);
+    }
+
+})
