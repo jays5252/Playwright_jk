67aa5c29e0cd190307d31ec4025a17edc3275b87
Jay <jay.aruven@gmail.com>
2025-03-31 11:49:59 +0530

    MouseEvent Practice

---
+++ tests/MouseEvent.spec.js
@@ -0,0 +1,28 @@
+import {test, expect} from '@playwright/test'
+
+test('Generic Click or mouse left click', async({page})=>
+{
+    await page.goto('https://uat.bldz.ai/');
+    await page.locator('[type="submit"]').click();
+    //await page.pause();
+})
+
+test('Double click via mouse on a Locator', async({page}) =>
+{
+    await page.goto('https://uat.bldz.ai/');
+     await page.locator('//h1[@class="text-2xl font-semibold"]').dblclick();
+    //await page.pause();
+})
+
+test('Right click on an element', async({page}) =>
+{
+    await page.goto('https://uat.bldz.ai/');
+    await page.locator('//h1[@class="text-2xl font-semibold"]').click({button:'right'});
+})
+test('Clicking on a button Shift+click', async({page}) =>
+{
+    await page.goto('https://uat.bldz.ai/');
+    await page.locator('//h1[@class="text-2xl font-semibold"]').click({modifiers:['Shift']});
+})
+
+
